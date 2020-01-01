de8298e61ab847f8b2b59426d0ee4a97f87c8cf2
Nathan Leong <nathan.wll@hotmail.com>
2020-01-01 15:21:10 +1100

    fixed build script

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
 instance_id=$1
 
-# aws ssm send-command \
-#     --document-name "AWS-RunShellScript" \
-#     --parameters commands=["sudo docker stop airbnbcontainer && sudo docker rm airbnbcontainer"] \
-#     --instance-ids $instance_id \
-#     --comment "stop containers" \
-#     --output text
-
-# sleep 15;
-# aws ssm send-command \
-#     --document-name "AWS-RunShellScript" \
-#     --parameters commands=["cd /home/ec2-user/airbnb && git pull origin master"] \
-#     --instance-ids "i-02802fe96404ca8c3" \
-#     --comment "pull git repo" \
-#     --output text
-
-#sleep 15;
 aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --parameters commands=["cd /home/ec2-user/airbnb && bash remotescript.sh"] \
